b1bb7532facfe84b77345ea3d8cd3baa5c193627
Martin Nguyen <martin@maddin.io>
2017-09-05 17:49:09 +0200

    Added searchBar.

--- Gitsuketa/ViewController.swift
+++ Gitsuketa/ViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let searchResultsViewController = SearchResultsViewController()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
+        let searchController = UISearchController(searchResultsController: searchResultsViewController)
+        let searchBar = searchController.searchBar
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(searchBar)
+        searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
     }
 
 }
