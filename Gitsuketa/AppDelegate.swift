095f3c4560d13d52e12d84dedaf91949a6e81f10
Martin Nguyen <martin@maddin.io>
2017-09-05 13:43:11 +0200

    Setup project to not rely on storyboards.

--- Gitsuketa/AppDelegate.swift
+++ Gitsuketa/AppDelegate.swift
@@ -16,6 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        let viewController = ViewController()
+        let rootNavigationController = UINavigationController(rootViewController: viewController)
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = rootNavigationController
+        window.backgroundColor = UIColor.white
+        window.makeKeyAndVisible()
+        self.window = window
+
         return true
     }
 
