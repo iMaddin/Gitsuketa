781b45cbceff21c26693e64c9af01ea2d9833d12
Martin Nguyen <martin@maddin.io>
2017-09-06 02:15:52 +0200

    Added UISearchController.

--- Gitsuketa/ViewController.swift
+++ Gitsuketa/ViewController.swift
@@ -11,18 +11,33 @@ import UIKit
 class ViewController: UIViewController {
 
     let searchResultsViewController = SearchResultsViewController()
+    let resultsDataSource = SearchResultsDataSource()
+    var searchController: UISearchController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
+        view.accessibilityIdentifier = "ViewController view"
+        searchResultsViewController.view.accessibilityIdentifier = "searchResultsViewController.view"
+
+        searchResultsViewController.tableView.dataSource = resultsDataSource
+
         let searchController = UISearchController(searchResultsController: searchResultsViewController)
+        self.searchController = searchController
+        searchController.searchResultsUpdater = self
+
+        let searchBarContainerView = UIView() // add container view to fix strange resizing behaviour of searchbar
+        searchBarContainerView.accessibilityIdentifier = "searchBarContainerView"
+        searchBarContainerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(searchBarContainerView)
+        searchBarContainerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        searchBarContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        searchBarContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        searchBarContainerView.heightAnchor.constraint(equalToConstant: 147).isActive = true
+
         let searchBar = searchController.searchBar
-        searchBar.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(searchBar)
-        searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        searchBarContainerView.addSubview(searchBar)
     }
 
 }
