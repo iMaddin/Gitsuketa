d24f0e469bbbd8621b6406fd09f0130caceb7c17
Martin Nguyen <martin@maddin.io>
2017-09-06 11:56:19 +0200

    Added url property. Added unit test.

--- Gitsuketa/GitHub/GitHubSearchParameter.swift
+++ Gitsuketa/GitHub/GitHubSearchParameter.swift
@@ -11,31 +11,58 @@ import Foundation
 let searchQuery = "https://api.github.com/search/repositories?"
 //"q=tetris+language:assembly&sort=stars&order=desc"
 
-struct GitHubSearchParameter {
-    let query: GitHubSearchQuery
-    let sort: GitHubSort
-    let order: GitHubOrder
+public struct GitHubSearchParameter {
+
+    public var url: URL? {
+        let q = "q=\(query.query)"
+        let sort = "sort=\(self.sort.rawValue)"
+        let order = "order=\(self.order.rawValue)"
+        let url = searchQuery + q + "&" + sort + "&" + order
+        return URL(string: url)
+    }
+
+    public let query: GitHubSearchQuery
+    public let sort: GitHubSort
+    public let order: GitHubOrder
+
+    public init(query: GitHubSearchQuery, sort: GitHubSort = .match, order: GitHubOrder = .descending) {
+        self.query = query
+        self.sort = sort
+        self.order = order
+    }
+
 }
 
-struct GitHubSearchQuery {
-    let query: String
-//    let created: Date
-//    let pushed: Date
-//    let fork:
-//    let forks:
-//    let in:
-//    let language:
-//    let repo:
-//    let size:
-//    let stars:
-//    let topic:
+public struct GitHubSearchQuery {
+
+    var query: String {
+        return keyword
+    }
+
+    public let keyword: String
+//    let created: Date? //  ISO8601
+//    let pushed: Date?
+//    let fork: String?
+//    let forks: String?
+//    let searchFields: String?
+//    let language: String?
+//    let repo: String?
+//    let user: String?
+//    let size: Int?
+//    let stars: Int?
+//    let topic: [String?]
+
+    public init(keyword: String) {
+        self.keyword = keyword
+    }
+
 }
 
-enum GitHubSort: String {
+public enum GitHubSort: String {
     case match, stars, forks, updated
 }
 
-enum GitHubOrder: String {
+public enum GitHubOrder: String {
     case ascending = "asc"
     case descending = "desc"
 }
---
+++ GitsuketaTests/GitHubSearchParameterTests.swift
@@ -0,0 +1,49 @@
+//
+//  GitHubSearchParameterTests.swift
+//  GitsuketaTests
+//
+//  Created by Maddin on 06.09.17.
+//  Copyright © 2017 Maddin. All rights reserved.
+//
+
+import XCTest
+import Gitsuketa
+
+class GitHubSearchParameterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
+
+extension GitHubSearchParameterTests {
+
+    func testBasicQuery() {
+        let searchKeyword = "tetris"
+        let searchQuery = GitHubSearchQuery(keyword: searchKeyword)
+        let searchParameter = GitHubSearchParameter(query: searchQuery)
+
+        let expectation = "https://api.github.com/search/repositories?q=tetris&sort=match&order=desc"
+        XCTAssertEqual(searchParameter.url?.absoluteString, expectation)
+    }
+
+}
