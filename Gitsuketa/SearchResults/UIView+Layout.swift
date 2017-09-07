81aaec9c8bd5e4313a38101be42630c5feae00cd
Martin Nguyen <martin@maddin.io>
2017-09-07 18:08:19 +0200

    Added UView+Layout.

---
+++ Gitsuketa/SearchResults/UIView+Layout.swift
@@ -0,0 +1,20 @@
+//
+//  UIView+Layout.swift
+//  Gitsuketa
+//
+//  Created by Maddin on 07.09.17.
+//  Copyright © 2017 Maddin. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+
+    func constraints(equalToEdgeOf anotherView: UIView, constants: UIEdgeInsets = UIEdgeInsetsMake(0, 0, 0, 0)) {
+        self.topAnchor.constraint(equalTo: anotherView.topAnchor, constant: -constants.top).isActive = true
+        self.bottomAnchor.constraint(equalTo: anotherView.bottomAnchor, constant: constants.bottom).isActive = true
+        self.leadingAnchor.constraint(equalTo: anotherView.leadingAnchor, constant: -constants.left).isActive = true
+        self.trailingAnchor.constraint(equalTo: anotherView.trailingAnchor, constant: constants.right).isActive = true
+    }
+
+}
