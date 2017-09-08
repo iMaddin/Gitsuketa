0954c0718d4a91a85925fea16a88fd3319237e7f
Martin Nguyen <martin@maddin.io>
2017-09-08 19:37:10 +0200

    Added RangeSelectionViewController.

---
+++ Gitsuketa/SearchResults/RangeSelectionViewController.swift
@@ -0,0 +1,82 @@
+//
+//  RangeSelectionViewController.swift
+//  Gitsuketa
+//
+//  Created by Maddin on 08.09.17.
+//  Copyright © 2017 Maddin. All rights reserved.
+//
+
+import UIKit
+
+class RangeSelectionViewController: UIViewController {
+
+    // MARK: - Properties
+
+    var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+
+    var rangeQualifierButton: UIButton = {
+        let rangeQualifierButton = UIButton()
+        rangeQualifierButton.setTitle(NSLocalizedString("=", comment: "Select Date placeholder text"), for: .normal)
+        rangeQualifierButton.setTitleColor(UIColor.blue, for: .normal)
+        return rangeQualifierButton
+    }()
+
+    var rangeQualifierPickerView: UIPickerView = {
+        let rangeQualifierPickerView = UIPickerView()
+        rangeQualifierPickerView.accessibilityIdentifier = " dateRange SegmentedControl"
+        return rangeQualifierPickerView
+    }()
+
+    var rangeQualifierPickerManager: RangeQualifierPickerManager?
+
+    // MARK: - RangeSelectionViewController
+
+    let leftView: UIView
+    let rightView: UIView
+
+    init(leftView: UIView, rightView: UIView) {
+        self.leftView = leftView
+        self.rightView = rightView
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    // MARK: - UIViewController
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.addSubview(stackView)
+        view.constraints(equalToEdgeOf: stackView)
+
+        stackView.addArrangedSubview(rangeQualifierButton)
+        stackView.addArrangedSubview(rightView)
+
+        rangeQualifierPickerManager = RangeQualifierPickerManager(button: rangeQualifierButton)
+        rangeQualifierPickerManager?.didSelectBetweenRangeQualifier = { flag in self.didSelectBetweenRangeQualifier(flag: flag)}
+
+        rangeQualifierPickerView.dataSource = rangeQualifierPickerManager
+        rangeQualifierPickerView.delegate = rangeQualifierPickerManager
+    }
+
+}
+
+fileprivate extension RangeSelectionViewController {
+
+    func didSelectBetweenRangeQualifier(flag: Bool) {
+        if flag {
+            stackView.insertArrangedSubview(leftView, at: 0)
+        } else {
+            rangeQualifierButton.removeFromSuperview()
+            stackView.removeArrangedSubview(leftView)
+        }
+    }
+
+}
