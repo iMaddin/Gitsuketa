32db8bf801009bb8ec5baa4afe9e756d6eee3b54
Martin Nguyen <martin@maddin.io>
2017-09-08 10:58:15 +0200

    Added GitHubStringQualifier.

---
+++ Gitsuketa/GitHub/GitHubStringQualifier.swift
@@ -0,0 +1,13 @@
+//
+//  GitHubStringQualifier.swift
+//  Gitsuketa
+//
+//  Created by Maddin on 08.09.17.
+//  Copyright © 2017 Maddin. All rights reserved.
+//
+
+import Foundation
+
+protocol GitHubStringQualifier {
+    var stringQualifier: String { get }
+}
