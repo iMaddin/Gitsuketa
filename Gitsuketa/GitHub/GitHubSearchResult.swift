9d44e83b66952c3a7638f8095531b2dc303dd942
Martin Nguyen <martin@maddin.io>
2017-09-09 22:39:35 +0200

    Added more GitHubSearchResult properties.

--- Gitsuketa/GitHub/GitHubSearchResult.swift
+++ Gitsuketa/GitHub/GitHubSearchResult.swift
@@ -9,22 +9,65 @@
 import Foundation
 
 struct GitHubSearchResultItem: Codable {
-    let fullName: String
-    let descriptionText: String?
-    let url: String
-    let language: String?
-    let updatedAt: String
-    let stargazersCount: Int
 
-//    var readmeURL: URL?
+    struct GitHubOwner: Codable {
+        let login: String?
+    }
+
+    let createdAt: String?
+    let defaultBranch: String?
+    let descriptionText: String?
+    let fork: Bool?
+    let forks: Int?
+    let forksCount: Int?
+    let fullName: String?
+    let hasDownloads: Bool?
+    let hasIssues: Bool?
+    let hasPages: Bool?
+    let hasProjects: Bool?
+    let hasWiki: Bool?
+    let language: String?
+    let name: String?
+    let openIssues: Int?
+    let openIssuesCount: Int?
+    let owner: GitHubOwner?
+    let isPrivate: Bool?
+    let pushedAt: String?
+    let score: Float?
+    let size: Int?
+    let stargazersCount: Int?
+    let updatedAt: String?
+    let url: String?
+    let watchers: Int?
+    let watchersCount: Int?
 
     enum CodingKeys: String, CodingKey {
-        case fullName = "full_name"
+        case createdAt = "created_at"
+        case defaultBranch = "default_branch"
         case descriptionText = "description"
-        case url = "html_url"
-        case language
-        case updatedAt = "updated_at"
+        case fork = "fork"
+        case forks = "forks"
+        case forksCount = "forks_count"
+        case fullName = "full_name"
+        case hasDownloads = "has_downloads"
+        case hasIssues = "has_issues"
+        case hasPages = "has_pages"
+        case hasProjects = "has_projects"
+        case hasWiki = "has_wiki"
+        case language = "language"
+        case name = "name"
+        case openIssues = "open_issues"
+        case openIssuesCount = "open_issues_count"
+        case owner = "owner"
+        case isPrivate = "private"
+        case pushedAt = "pushed_at"
+        case score = "score"
+        case size = "size"
         case stargazersCount = "stargazers_count"
+        case updatedAt = "updated_at"
+        case url = "html_url"
+        case watchers = "watchers"
+        case watchersCount = "watchers_count"
     }
 
 }
