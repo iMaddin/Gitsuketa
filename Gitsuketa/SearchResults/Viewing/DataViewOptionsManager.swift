a7bf5ba0a3848fe11b52136216ce53ce3287df5d
Martin Nguyen <martin@maddin.io>
2017-09-10 12:40:18 +0200

    Added a function for getting a bool for a specific DataViewOption.

--- Gitsuketa/SearchResults/Viewing/DataViewOptionsManager.swift
+++ Gitsuketa/SearchResults/Viewing/DataViewOptionsManager.swift
@@ -37,5 +37,62 @@ struct DataViewOptionsManager {
     var url: Bool = false
     var watchers: Bool = false
     var watchersCount: Bool = false
+
+    func bool(forDataViewOption option: DataViewOptions) -> Bool {
+        switch option {
+        case .createdAt:
+            return createdAt
+        case .defaultBranch:
+            return defaultBranch
+        case .descriptionText:
+            return descriptionText
+        case .fork:
+            return fork
+        case .forks:
+            return forks
+        case .forksCount:
+            return forksCount
+        case .fullName:
+            return fullName
+        case .hasDownloads:
+            return hasDownloads
+        case .hasIssues:
+            return hasIssues
+        case .hasPages:
+            return hasPages
+        case .hasProjects:
+            return hasProjects
+        case .hasWiki:
+            return hasWiki
+        case .language:
+            return language
+        case .name:
+            return name
+        case .openIssues:
+            return openIssues
+        case .openIssuesCount:
+            return openIssuesCount
+        case .owner:
+            return owner
+        case .isPrivate:
+            return isPrivate
+        case .pushedAt:
+            return pushedAt
+        case .score:
+            return score
+        case .size:
+            return size
+        case .stargazersCount:
+            return stargazersCount
+        case .updatedAt:
+            return updatedAt
+        case .url:
+            return url
+        case .watchers:
+            return watchers
+        case .watchersCount:
+            return watchersCount
+        }
+    }
     
 }
