7ff186e768cdb329db3619f653fc519df3046e1d
Martin Nguyen <martin@maddin.io>
2017-09-10 15:27:53 +0200

    Added GitHubBasicAuthentication.

---
+++ Gitsuketa/GitHubBasicAuthentication.swift
@@ -0,0 +1,13 @@
+//
+//  GitHubBasicAuthentication.swift
+//  Gitsuketa
+//
+//  Created by Maddin on 10.09.17.
+//  Copyright © 2017 Maddin. All rights reserved.
+//
+
+import Foundation
+
+struct GitHubBasicAuthentication {
+    
+}
